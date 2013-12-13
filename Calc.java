19e0b48d3c7d674fe7119e6dd7fbd0abd211d101
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 20:23:35 +0200

    GREEN: test 0, 1 and 2 numbers

--- Calc.java
+++ Calc.java
@@ -2,6 +2,15 @@
 public class Calc {
 
 	public int add (String numbers) {
-		return -1;
+		int coma=numbers.indexOf(",");
+		if (numbers.isEmpty())
+			return 0;
+		if (coma == -1)
+			return Integer.parseInt(numbers);
+		else {
+			int n1 = Integer.parseInt(numbers.substring(0, coma));
+			int n2 = Integer.parseInt(numbers.substring(coma+1, numbers.length()));
+			return n1+n2;
+		}
 	}
 }
