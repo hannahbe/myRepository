ea3a5a3c4ce6d4edac249fb1c6d7f72f82715632
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 23:01:12 +0200

    BLUE: allow different delimiters

--- Calc.java
+++ Calc.java
@@ -1,46 +1,22 @@
 
 public class Calc {
 	
-	public static final char COMA = ',';
-	public static final char NEW_LINE = '\n';
+	public static final String DEFAULT_DELIMITER = "[,\n]";
+	public static final String CHANGE_DELIMITER = "//";
 
 	public int add (String numbers) {
 		if (numbers.isEmpty())
 			return 0;
-		int sum = 0, from = 0, coma, newLine, delimiter;
-		char delimiterChar = 0;
-		boolean userChooseDelimiter = false;
-		if (numbers.indexOf("//") == 0) {
-			userChooseDelimiter = true;
-			delimiterChar = numbers.charAt(2);
-			from = 4;
+		int sum = 0;
+		String delimiter = DEFAULT_DELIMITER;
+		if (numbers.startsWith(CHANGE_DELIMITER)) {
+			delimiter = numbers.substring (CHANGE_DELIMITER.length(), CHANGE_DELIMITER.length() + 1);
+			numbers = numbers.substring(CHANGE_DELIMITER.length()+2);
 		}
-		do {
-			if (!userChooseDelimiter) {
-				coma = numbers.indexOf(COMA, from);
-				newLine = numbers.indexOf(NEW_LINE, from);
-				if (coma == -1 && newLine == -1) {
-					delimiter = coma;
-					sum += Integer.parseInt(numbers.substring(from, numbers.length()));
-					break;
-				}
-				else if ((coma < newLine && coma != -1) || newLine == -1 && coma != -1)
-					delimiter = coma;
-				else
-					delimiter = newLine;
-				sum += Integer.parseInt(numbers.substring(from, delimiter));
-				from = delimiter+1;
-			}
-			else {
-				delimiter = numbers.indexOf(delimiterChar, from);
-				if (delimiter != -1) {
-					sum += Integer.parseInt(numbers.substring(from, delimiter));
-					from = delimiter+1;
-				}
-				else
-					sum += Integer.parseInt(numbers.substring(from, numbers.length()));
-			}
-		} while (delimiter != -1);
+		String nums[] = numbers.split (delimiter);
+		for (int i=0; i<nums.length; i++) {
+			sum += Integer.parseInt(nums[i]);
+		}
 		return sum;
 	}
 	
