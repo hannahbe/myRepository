90651dfac1d9f0e011467a5991040310106fe00a
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 22:36:35 +0200

    GREEN: test different delimiters

--- Calc.java
+++ Calc.java
@@ -8,20 +8,39 @@ public class Calc {
 		if (numbers.isEmpty())
 			return 0;
 		int sum = 0, from = 0, coma, newLine, delimiter;
+		char delimiterChar = 0;
+		boolean userChooseDelimiter = false;
+		if (numbers.indexOf("//") == 0) {
+			userChooseDelimiter = true;
+			delimiterChar = numbers.charAt(2);
+			from = 4;
+		}
 		do {
-			coma = numbers.indexOf(COMA, from);
-			newLine = numbers.indexOf(NEW_LINE, from);
-			if (coma == -1 && newLine == -1) {
-				sum += Integer.parseInt(numbers.substring(from, numbers.length()));
-				break;
+			if (!userChooseDelimiter) {
+				coma = numbers.indexOf(COMA, from);
+				newLine = numbers.indexOf(NEW_LINE, from);
+				if (coma == -1 && newLine == -1) {
+					delimiter = coma;
+					sum += Integer.parseInt(numbers.substring(from, numbers.length()));
+					break;
+				}
+				else if ((coma < newLine && coma != -1) || newLine == -1 && coma != -1)
+					delimiter = coma;
+				else
+					delimiter = newLine;
+				sum += Integer.parseInt(numbers.substring(from, delimiter));
+				from = delimiter+1;
 			}
-			else if ((coma < newLine && coma != -1) || newLine == -1 && coma != -1)
-				delimiter = coma;
-			else
-				delimiter = newLine;
-			sum += Integer.parseInt(numbers.substring(from, delimiter));
-			from = delimiter+1;
-		} while (coma != -1);
+			else {
+				delimiter = numbers.indexOf(delimiterChar, from);
+				if (delimiter != -1) {
+					sum += Integer.parseInt(numbers.substring(from, delimiter));
+					from = delimiter+1;
+				}
+				else
+					sum += Integer.parseInt(numbers.substring(from, numbers.length()));
+			}
+		} while (delimiter != -1);
 		return sum;
 	}
 	
