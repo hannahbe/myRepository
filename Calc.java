e37cd4c7d199da75728d55a60adfc706278982ef
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 23:31:25 +0200

    GREEN: throw exception

--- Calc.java
+++ Calc.java
@@ -3,20 +3,32 @@ public class Calc {
 	
 	public static final String DEFAULT_DELIMITER = "[,\n]";
 	public static final String CHANGE_DELIMITER = "//";
+	public static final String BASIC_EXCEPTION_MESSAGE = "negatives not allowed ";
 
-	public int add (String numbers) {
+	public int add (String numbers) throws Exception {
 		if (numbers.isEmpty())
 			return 0;
-		int sum = 0;
+		int sum = 0, num = 0;
 		String delimiter = DEFAULT_DELIMITER;
+		String exceptionMessage = BASIC_EXCEPTION_MESSAGE;
 		if (numbers.startsWith(CHANGE_DELIMITER)) {
 			delimiter = numbers.substring (CHANGE_DELIMITER.length(), CHANGE_DELIMITER.length() + 1);
 			numbers = numbers.substring(CHANGE_DELIMITER.length()+2);
 		}
 		String nums[] = numbers.split (delimiter);
 		for (int i=0; i<nums.length; i++) {
-			sum += Integer.parseInt(nums[i]);
+			num = Integer.parseInt(nums[i]);
+			if (num < 0) {
+				if (!exceptionMessage.equals(BASIC_EXCEPTION_MESSAGE))
+					exceptionMessage += ",";
+				exceptionMessage += num;
+			}
+			else
+				sum += num;
 		}
+		if (!exceptionMessage.equals(BASIC_EXCEPTION_MESSAGE))
+				throw new Exception(exceptionMessage);
+
 		return sum;
 	}
 	
--- CalcTest.java
+++ CalcTest.java
@@ -8,37 +8,37 @@ import org.junit.rules.ExpectedException;
 public class CalcTest {
 	
 	@Test
-	public void testEmptyString() {
+	public void testEmptyString() throws Exception {
 	int result = new Calc().add("");
 	assertEquals(0,result);
 	}
 	
 	@Test
-	public void testOneNumber() {
+	public void testOneNumber() throws Exception {
 		int result = new Calc().add("2");
 		assertEquals(2,result);
 	}
 	
 	@Test
-	public void testTwoNumbers() {
+	public void testTwoNumbers() throws Exception {
 		int result = new Calc().add("2,3");
 		assertEquals(5,result);
 	}
 	
 	@Test
-	public void testUnknownAmountOfNumbers() {
+	public void testUnknownAmountOfNumbers() throws Exception {
 		int result = new Calc().add("2,3,5");
 		assertEquals(10,result);
 	}
 	
 	@Test
-	public void testNewLinesBetweenNumbers() {
+	public void testNewLinesBetweenNumbers() throws Exception {
 		int result = new Calc().add("2\n3,5");
 		assertEquals(10,result);
 	}
 	
 	@Test
-	public void testSupportDifferentDelimiters() {
+	public void testSupportDifferentDelimiters() throws Exception {
 		int result = new Calc().add("//;\n2;3");
 		assertEquals(5,result);
 	}
@@ -47,7 +47,7 @@ public class CalcTest {
 	public ExpectedException exception = ExpectedException.none();
 	
 	@Test
-	public void testThrowException() {
+	public void testThrowException() throws Exception {
 		exception.expect(Exception.class);
 		exception.expectMessage("negatives not allowed -2,-3");
 		new Calc().add("-2\n-3,5");
