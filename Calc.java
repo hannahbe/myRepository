874b1f57282f1d232145cd15e5c2e95e20e4c7da
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 21:33:21 +0200

    BLUE: unknown amount of numbers

--- Calc.java
+++ Calc.java
@@ -4,14 +4,9 @@ public class Calc {
 	public static final char COMA = ',';
 
 	public int add (String numbers) {
-		int coma=numbers.indexOf(COMA);
 		if (numbers.isEmpty())
 			return 0;
-		if (coma == -1)
-			return Integer.parseInt(numbers);
-		int from = 0;
-		int sum = Integer.parseInt(numbers.substring(from, coma));
-		from = coma+1;
+		int sum = 0, from = 0, coma;
 		do {
 			if ((coma=numbers.indexOf(COMA, from)) != -1) {
 				sum += Integer.parseInt(numbers.substring(from, coma));
