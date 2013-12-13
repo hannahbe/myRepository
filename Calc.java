9f35203386cbf14cd175f8b2bf116786789d0a0a
hannahbe <hannah.bellaiche@gmail.com>
2013-12-13 21:48:31 +0200

    GREEN: test new lines between numbers

--- Calc.java
+++ Calc.java
@@ -2,13 +2,28 @@
 public class Calc {
 	
 	public static final char COMA = ',';
+	public static final char NEW_LINE = '\n';
 
 	public int add (String numbers) {
 		if (numbers.isEmpty())
 			return 0;
-		int sum = 0, from = 0, coma;
+		int sum = 0, from = 0, coma, newLine, delimiter;
 		do {
-			if ((coma=numbers.indexOf(COMA, from)) != -1) {
+			coma = numbers.indexOf(COMA, from);
+			newLine = numbers.indexOf(NEW_LINE, from);
+			if (coma != -1 && newLine != -1) {
+				if (coma < newLine)
+					delimiter = coma;
+				else
+					delimiter = newLine;
+				sum += Integer.parseInt(numbers.substring(from, delimiter));
+				from = delimiter+1;
+			}
+			else if (coma == -1 && newLine != -1) {
+				sum += Integer.parseInt(numbers.substring(from, newLine));
+				from = newLine+1;
+			}
+			else if (newLine == -1 && coma != -1) {
 				sum += Integer.parseInt(numbers.substring(from, coma));
 				from = coma+1;
 			}
